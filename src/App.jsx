0ce86fa487b4cdf306b6c2e80cef8751339bac8f
zhangkx <zhangkx@winnerinf.com>
2023-01-04 18:01:43 +0800

    feat: 一字题demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import GameTicTacToe from './components/tic-tac-toe'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -27,6 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h3>tic-tac-toe 🐱‍🏍</h3>
+      <GameTicTacToe />
     </div>
   )
 }
---
+++ src/components/tic-tac-toe.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react'
+import { square, boardRow, statusStyle, game, gameInfo, liMove } from './ticTacToe.module.css'
+function Square({ value, onSquareClick }) {
+  return <button className={square} onClick={onSquareClick}>{value}</button>
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) return
+    const nextSquares = squares.slice()
+    if (xIsNext) {
+      nextSquares[i] = 'x'
+    } else {
+      nextSquares[i] = 'o'
+    }
+    onPlay(nextSquares)
+  }
+  const winner = calculateWinner(squares)
+  let status;
+  if (winner) {
+    status = `Winner is: ${winner}`
+  } else {
+    status = `Next player: ${xIsNext ? 'x' : 'o'}`
+  }
+  return <>
+    <div className={statusStyle}>{status}</div>
+    <div className={boardRow}>
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
+    </div>
+    <div className={boardRow}>
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
+    </div>
+    <div className={boardRow}>
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
+    </div>
+  </>
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+export default function GameTicTacToe() {
+  const [currentMove, setCurrentMove] = useState(0)
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const currentSquares = history[currentMove]
+  const xIsNext = currentMove % 2 === 0
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1)
+  }
+  function jumpTo(move) {
+    setCurrentMove(move)
+  }
+  const move = history.map((squares, move) => {
+    let description
+    if (move > 0) {
+      description = `Go to move #${move}`
+    } else {
+      description = 'Go to game start'
+    }
+    return <li key={move} className={liMove}>
+      <button onClick={() => jumpTo(move)}>{description}</button>
+    </li>
+  })
+  return <div className={game}>
+    <div>
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}></Board>
+    </div>
+    <div className={gameInfo}>
+      <ol>{move}</ol>
+    </div>
+  </div>
+}
