9ee929d7f37ffd2390a616b5723b1c321b868137
mianmalife <253590433@qq.com>
2023-09-10 17:06:23 +0800

    feat: 歌词适配主题

--- src/components/audioPlayer/index.jsx
+++ src/components/audioPlayer/index.jsx
@@ -1,10 +1,13 @@
 import { useRef, useEffect } from "react";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 import "./index.css";
 import hum from "./daoxiang.js";
 import { transformLrc, findLrcIndex, offsetNum } from "./scrollLrc.js";
 const lrc = transformLrc(hum);
 
 export default function AudioPlayer() {
+  const theme = useContext(ThemeContext);
   const containerRef = useRef();
   const audioRef = useRef(0);
   const ulRef = useRef(0);
@@ -18,10 +21,10 @@ export default function AudioPlayer() {
     let y = offsetNum(index);
     ulRef.current.style.transform = `translateY(${y}px)`;
     if (document.querySelector(".active")) {
-      document.querySelector(".active").classList = "";
+      document.querySelector(".active").classList.remove("active");
     }
     if (ulRef.current.children[index].textContent) {
-      ulRef.current.children[index].classList = "active";
+      ulRef.current.children[index].classList.add("active");
     }
   }
   return (
@@ -35,7 +38,19 @@ export default function AudioPlayer() {
       <div className="lrc__container">
         <ul ref={ulRef}>
           {lrc.map((item, index) => (
-            <li key={index} data-value={item.words}>
+            <li
+              key={index}
+              data-value={item.words}
+              className="light"
+              ref={(ele) => {
+                if (ele) {
+                  ele.className.includes("active")
+                    ? (ele.classList = "active")
+                    : (ele.classList = "");
+                  ele.classList.add(theme);
+                }
+              }}
+            >
               {item.words}
             </li>
           ))}
